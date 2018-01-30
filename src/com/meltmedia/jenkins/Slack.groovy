e6c0075b5034071bdaa076de2f5901522957b118
chrishaley42 <chris.haley@meltmedia.com>
2018-01-30 10:12:48 -0700

    adding Slack class
    making method static
    
    add env as param
    
    add env as param
    
    updating to not use env
    
    moving url to the first param
    
    removed static
    
    adding steps to constructor
    
    standardizing

---
+++ src/com/meltmedia/jenkins/Slack.groovy
@@ -0,0 +1,30 @@
+package com.meltmedia.jenkins;
+
+class Slack {
+    def script
+    Slack(script) {this.script = script}
+    def slackNotifyBuild(String url, String buildStatus = 'STARTED', String slackChannel = '#build-alerts') {
+
+        // Default values
+        def colorName = 'RED'
+        def colorCode = '#FF0000'
+        def summary = "${buildStatus}: ${url}"
+
+        // Override default values based on build status
+        if (buildStatus == 'STARTED') {
+            // color = 'YELLOW'
+            colorCode = '#FFFF00'
+        } else if (buildStatus == 'SUCCESSFUL') {
+            // color = 'GREEN'
+            colorCode = '#00FF00'
+        } else {
+            // color = 'RED'
+            colorCode = '#FF0000'
+        }
+
+        // Send notifications
+        script.slackSend (color: colorCode, message: summary, channel: slackChannel)
+    }
+
+
+}
